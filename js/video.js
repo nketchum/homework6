ce21dcc734a59b13cca0e805496323978198e53b
Nicholas Ketchum <nketchum@gmail.com>
2020-11-19 22:17:58 -0500

    add volume label on play

--- js/video.js
+++ js/video.js
@@ -7,6 +7,8 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
   video.play()
+  var label = document.getElementById('volume');
+  label.innerHTML = (video.volume * 100) + "%"
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
